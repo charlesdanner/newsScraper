afad846075e8494c6cd06c8194777bd14a402e62
Charles Danner <danner.ced1@gmail.com>
2019-11-18 14:05:29 -0500

    moved files so handlebars actually works now.

--- server.js
+++ server.js
@@ -2,26 +2,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
+const exphbs = require("express-handlebars")
 
 const db = require("./models")
 
-const PORT = 3000;
-
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-app.use(express.static("public"));
+
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
 
-
+app.get("/", (req, res) => {
+    res.render("articles")
+})
 
 
 
